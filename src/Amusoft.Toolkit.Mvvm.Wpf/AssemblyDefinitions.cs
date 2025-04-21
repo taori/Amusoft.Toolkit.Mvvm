570d51d297f37a69f7740c9b5ca05dcbb9845e11
Andreas Müller <taori@users.noreply.github.com>
2025-04-21 04:07:38 +0200

    documentation + xmlns (#8)

---
+++ src/Amusoft.Toolkit.Mvvm.Wpf/AssemblyDefinitions.cs
@@ -0,0 +1,6 @@
+﻿// This file is licensed to you under the MIT license.
+
+using System.Windows.Markup;
+
+[assembly:XmlnsDefinition("http://schemas.amusoft.com/wpf/2025/xaml","Amusoft.Toolkit.Mvvm.Wpf")]
+[assembly:XmlnsPrefix("http://schemas.amusoft.com/wpf/2025/xaml", "amumvvm")]
